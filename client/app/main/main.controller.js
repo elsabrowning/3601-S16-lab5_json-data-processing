1dddd1b4af817630ef04a5b012fb423405c4e0a5
mitch723 <mitch723@morris,umn.edu>
2016-02-22 20:19:15 -0600

    (Matt, Michelle) Made a sort function that isn't terribly broken.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,34 +8,64 @@
     constructor($http, $scope, socket) {
       this.$http = $http;
       this.awesomeThings = [];
+      this.boxcheck = 0;
 
       $http.get('/api/students').then(response => {
         this.awesomeThings = response.data;
-      socket.syncUpdates('student', this.awesomeThings);
-    });
+        socket.syncUpdates('student', this.awesomeThings);
+      });
 
-    $scope.$on('$destroy', function() {
-    socket.unsyncUpdates('student');
-  });
-}
+      $scope.$on('$destroy', function() {
+        socket.unsyncUpdates('student')
+        ;
+      });
+    }
 
-addThing() {
-  if (this.newThing) {
-    this.$http.post('/api/students', { name: this.newThing });
-    this.newThing = '';
+    addThing()
+    {
+      if (this.newThing) {
+        this.$http.post('/api/students', {name: this.newThing});
+        this.newThing = '';
+      }
+    }
+
+    deleteThing(thing)
+    {
+      this.$http.delete('/api/students/' + thing._id);
+    }
+
+    sumCredits() {
+      var totalcredits = 0;
+      for (i = 0; i < length; i++) {
+        totalcredits += this.courses[i].course.credits
+      }
+      return totalcredits;
+    }
+
+
+    orderName() {
+      this.boxcheck=1;
+      console.log("orderName");
+    };
+
+    orderDOB() {
+      this.boxcheck=2;
+      console.log("orderDOB");
+    };
+
+
+    orderByFunction()
+    {
+      if (this.boxcheck == 1) {
+        return 'lastName';
+      } else{
+        console.log("Time to cry about the order by");
+      }
+    };
+
   }
-}
 
-deleteThing(thing) {
-  this.$http.delete('/api/students/' + thing._id);
-}
-}
-
-sortAlphabet(){
-  this.$http.sort();
-}
-
-angular.module('3601S16Lab5JsonDataProcessingApp')
-  .controller('MainController', MainController);
+  angular.module('3601S16Lab5JsonDataProcessingApp')
+    .controller('MainController', MainController);
 
 })();
