762da5270b907f7ac4fa836f26b875353926468c
mitch723 <mitch723@morris,umn.edu>
2016-02-22 18:24:13 -0600

    (Matt, Michelle, Luke, Elsa) API displays on html page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,33 +1,34 @@
+
 'use strict';
 
 (function() {
 
-class MainController {
+  class MainController {
 
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.awesomeThings = [];
+    constructor($http, $scope, socket) {
+      this.$http = $http;
+      this.awesomeThings = [];
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
+      $http.get('/api/students').then(response => {
+        this.awesomeThings = response.data;
+      socket.syncUpdates('student', this.awesomeThings);
     });
 
     $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
+    socket.unsyncUpdates('student');
+  });
+}
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
+addThing() {
+  if (this.newThing) {
+    this.$http.post('/api/students', { name: this.newThing });
+    this.newThing = '';
   }
+}
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
+deleteThing(thing) {
+  this.$http.delete('/api/students/' + thing._id);
+}
 }
 
 angular.module('3601S16Lab5JsonDataProcessingApp')
