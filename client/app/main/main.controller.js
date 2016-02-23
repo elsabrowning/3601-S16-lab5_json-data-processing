60ea61b67b33cb1de8044c27edef98d80b80d774
brow3924 <brow3924@morris.umn.edu>
2016-02-22 19:16:26 -0600

    Fixed bugs (Luke, Elsa, JACK)

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -11,29 +11,31 @@
 
       $http.get('/api/students').then(response => {
         this.awesomeThings = response.data;
-      socket.syncUpdates('student', this.awesomeThings);
-    });
+        socket.syncUpdates('student', this.awesomeThings);
+      });
 
-    $scope.$on('$destroy', function() {
-    socket.unsyncUpdates('student');
-  });
-}
+      $scope.$on('$destroy', function () {
+        socket.unsyncUpdates('student');
+      });
+    }
 
-addThing() {
-  if (this.newThing) {
-    this.$http.post('/api/students', { name: this.newThing });
-    this.newThing = '';
+    addThing() {
+      if (this.newThing) {
+        this.$http.post('/api/students', {name: this.newThing});
+        this.newThing = '';
+      }
+    }
+
+    deleteThing(thing) {
+      this.$http.delete('/api/students/' + thing._id);
+    }
+
+
+    sortAlphabet() {
+      this.$http.sort();
+    }
+
   }
-}
-
-deleteThing(thing) {
-  this.$http.delete('/api/students/' + thing._id);
-}
-}
-
-sortAlphabet(){
-  this.$http.sort();
-}
 
 angular.module('3601S16Lab5JsonDataProcessingApp')
   .controller('MainController', MainController);
