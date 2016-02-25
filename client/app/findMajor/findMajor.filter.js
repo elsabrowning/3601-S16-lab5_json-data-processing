5e22cd5887642a5f7097831a16623e1d188dd11a
mitch723 <mitch723@morris,umn.edu>
2016-02-25 13:04:19 -0600

    (Michelle, Matt, Dan) Fixed orderby for Total Credits

--- client/app/findMajor/findMajor.filter.js
+++ client/app/findMajor/findMajor.filter.js
@@ -4,17 +4,20 @@ angular.module('3601S16Lab5JsonDataProcessingApp')
   .filter('findMajor', function () {
 
 
-    var calculateGPA = function(data, major){ //data.courses[i]
+    var checkMajor = function(data, major){ //data.courses[i]
       for(var i=0; i < data.length; i++) {
-        if (major == data[i].major1 || major == data[i].major2) {
-         return true;
-        } else {
-          return false;
+        if (major == data[i].major1) {
+         return data[i].major1;
+        }
+        if (major == data[i].major2) {
+          return data[i].major1;
+        }
+        else {
+          return "";
         }
       }
     };
 
-    if calculateGPA()
 
     return function (input) {
       return 'findMajor filter: ' + input;
