b6d02d937b816eededbd48d3b051cd7ddf4d94b2
Michelle King <michellelking@me.com>
2016-02-28 22:52:31 -0600

    (Michelle, Matt) Detailed Course information now appears for each student.

--- client/app/courseList/courseList.filter.js
+++ client/app/courseList/courseList.filter.js
@@ -7,7 +7,12 @@ angular.module('3601S16Lab5JsonDataProcessingApp')
       var coursesArray = [];
       var courseIndex = 0;
       for (courseIndex = 0; courseIndex < studentData.courses.length; courseIndex++) {
-          coursesArray.push(" " + studentData.courses[courseIndex].course.name);
+          coursesArray.push(
+            " " + studentData.courses[courseIndex].course.subject +
+            " " + studentData.courses[courseIndex].course.courseNumber +
+            ": " + studentData.courses[courseIndex].course.name +
+            " (" + studentData.courses[courseIndex].course.credits +
+            ") [" + studentData.courses[courseIndex].grade + "]");
       }
       return coursesArray;
     };
---
+++ client/app/courseList/courseList.filter.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Filter: courseList', function () {
+
+  // load the filter's module
+  beforeEach(module('3601S16Lab5JsonDataProcessingApp'));
+
+  // initialize a new instance of the filter before each test
+  var courseList;
+  beforeEach(inject(function ($filter) {
+    courseList = $filter('courseList');
+  }));
+
+  it('should return the input prefixed with "courseList filter:"', function () {
+    var text = 'angularjs';
+    expect(courseList(text)).toBe('courseList filter: ' + text);
+  });
+
+});
